0dc5074b6ac6d30baeb93fd92328af7f28cc949c
Ryoga <lingjia843@gmail.com>
2026-05-16 13:35:59 +0900

    all done

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import Layout, { siteTitle } from "@/components/Layout";
+import Link from "next/link";
+import utilStyle from '../styles/utils.module.css';
+import styles from '../styles/Home.module.css';
+import { getPostsData } from "@/lib/post";
+
+//SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getPostsData(); //id, title, date, thumbnail
+  console.log(allPostsData);
+  return {
+    props: {
+      allPostsData,
+    }
+  }
+}
+
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyle.headingMd}>
+        <p>プロフィール</p>
+      </section>
+
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({id, title, date, thumbnail}) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={thumbnail} className={styles.thumbnailImage} />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <p className={utilStyle.boldText}>{title}</p>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from "@/components/Layout";
+import { getAllPostsIds, getPostData } from "@/lib/post";
+import utilStyle from '../../styles/utils.module.css';
+import Head from "next/head";
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingX1}>{postData.title}</h1>
+        
+        <div className={utilStyle.lightText}>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{__html: postData.blogContentHTML}} />
+      </article>
+    </Layout>
+  );
+}
